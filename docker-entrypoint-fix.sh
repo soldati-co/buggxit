3e86680c375f22d09d145ee1a230de5c829edfaf
Nkosi2000 <nkosiphila901@gmail.com>
2026-05-19 14:32:19 +0200

    test continued

--- docker-entrypoint-fix.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Fix DNS before starting the main process
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-
-# Hand off to the original serversideup entrypoint
-exec /init
