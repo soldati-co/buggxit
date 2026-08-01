627e2503e3b66da95be1bc0431e9fe31219551c3
Nkosi2000 <nkosiphila901@gmail.com>
2026-08-01 16:18:23 +0200

    index

--- public/index.php
+++ public/index.php
@@ -1,11 +1,5 @@
 <?php
 
-if (env('APP_DEBUG', false)) {
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-}
-
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
