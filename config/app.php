b5b1fff846211385578873083dfb7838c5c178c3
Nkosi2000 <nkosiphila901@gmail.com>
2026-08-08 13:18:36 +0200

    up

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Buggxit Couture - Est 2018'),
+    'name' => env('APP_NAME', "Buggxit Couture"),
 
     /*
     |--------------------------------------------------------------------------
@@ -26,7 +26,8 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'production'),
+    // 'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'local'),
 
     /*
     |--------------------------------------------------------------------------
